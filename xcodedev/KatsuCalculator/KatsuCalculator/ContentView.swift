af267c6ca1c6e1b2e355adeebd549b9c801c0159
Colin Casey <colincas37@gmail.com>
2023-06-09 00:27:41 -0700

    added swift code

--- xcodedev/KatsuCalculator/KatsuCalculator/ContentView.swift
+++ xcodedev/KatsuCalculator/KatsuCalculator/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var resultText = "Result will be displayed here."
+    @State private var amount = ""
+    @State private var resultText = "Please enter the amount of chicken in grams."
     
     var body: some View {
         VStack {
-            Button("Test Communication") {
-                testCommunication()
+            TextField("Enter amount in grams", text: $amount)
+                .padding()
+                .keyboardType(.decimalPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Enter") {
+                convertToKilograms()
             }
             .padding()
             
@@ -23,19 +29,23 @@ struct ContentView: View {
         .padding()
     }
     
-    func testCommunication() {
-        guard let url = URL(string: "http://127.0.0.1:5000/test") else { return }
+    func convertToKilograms() {
+        guard let url = URL(string: "http://127.0.0.1:5000/convert") else { return }
+        guard let amountValue = Double(amount) else { return }
         
+        let bodyData = ["amount": amountValue]
         var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: bodyData, options: [])
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
                 do {
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String],
-                       let message = json["message"] {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                       let result = json["result"] {
                         DispatchQueue.main.async {
-                            self.resultText = message
+                            self.resultText = "Converted amount: \(result) kilograms"
                         }
                     }
                 } catch {
